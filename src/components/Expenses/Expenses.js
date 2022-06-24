ef54a4bc77a5e665e7141361d31a48e1c3ee2f0a
hong <hcy1997@naver.com>
2022-06-25 01:34:31 +0900

    2022/06/24 study

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,44 +1,31 @@
-import { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
+import {useState} from 'react';
 import Card from "../UI/Card";
 import ExpensesFilter from './ExpenseFilter';
 import './Expenses.css';
+import ExpenseList from "./ExpenseList";
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState('2020')
+    const [filteredYear, setFilteredYear] = useState('2020')
 
-  const filterChandgeHandler = (selectedYear) => {
-    console.log('expenese')
-    setFilteredYear(selectedYear)
-  }
+    const filterChandgeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear)
+    }
 
-  return (
-    <div>
-    <Card className="expenses">
-      <ExpensesFilter selected={ filteredYear } onChangeFilter={ filterChandgeHandler } />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      ></ExpenseItem>
-    </Card>
-    </div>
-  );
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear
+    })
+
+    return (
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter
+                    selected={filteredYear}
+                    onChangeFilter={filterChandgeHandler}
+                />
+                <ExpenseList items={filteredExpenses} />
+            </Card>
+        </div>
+    );
 }
 
 export default Expenses;
